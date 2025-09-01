de0abfd70848d038d55a17c43dd7d9b32d5b51e3
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-09-01 19:51:24 -0300

    bug: bug fixes for deploy

--- src/components/ui/container-text-flip.tsx
+++ src/components/ui/container-text-flip.tsx
@@ -33,7 +33,7 @@ export function ContainerTextFlip({
   const updateWidthForWord = () => {
     if (textRef.current) {
       // Add some padding to the text width (30px on each side)
-      // @ts-ignore - code 30 caracteres
+      // @ts-expect-error: scrollWidth pode não estar presente em textRef.current
       const textWidth = textRef.current.scrollWidth + 30;
       setWidth(textWidth);
     }
