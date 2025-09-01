c9599da0bab559b9053a6875bfa4dfffdd00a020
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-08-31 22:55:57 -0300

    chore: initial structure

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["res.cloudinary.com"], // ✅ permite imagens do Cloudinary
+  },
 };
 
 export default nextConfig;
