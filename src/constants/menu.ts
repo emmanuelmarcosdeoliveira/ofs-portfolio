219374369b162f18c56658c216cc34a80a3ec849
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-09-05 16:53:31 -0300

    chore: created constants to facilitate the construction of components

---
+++ src/constants/menu.ts
@@ -0,0 +1,7 @@
+export const linksMenu = {
+  pages: [
+    { id: "1", name: "Home", href: "/" },
+    { id: "2", name: "Projetos", href: "/projetos" },
+    { id: "3", name: "Sobre", href: "/sobre" },
+  ],
+};
---
+++ src/constants/works.ts
@@ -0,0 +1,90 @@
+export const works = [
+  {
+    id: 1,
+    role: "Gerente de Negócios",
+    company: "Arcos Dourados - Mc Donald's",
+    startDate: "2015-01",
+    endDate: "2022-05",
+    workType: "Integral",
+    location: "São Paulo, SP",
+    companyImage: "/logo-arcos.png",
+    description:
+      "Como gestor direto do restaurante liderei equipes de 12 gerentes e 60 funcionários Responsável por gerir diversos Indicadores como Turnover, Vendas, Margem Bruta, Treinamento garantindo excelência operacional e resultados financeiros.",
+    resultTitles: "Principais Conquistas:",
+    results: [
+      {
+        primary:
+          "+18% no aumento de vendas através de estratégias de ticket médio e ações promocionais.",
+      },
+      {
+        secondary:
+          "Redução de custos em 15% com controle de MDO, margem bruta e otimização de processos.",
+      },
+      {
+        tertiary:
+          "Turnover reduzido em 20% com programas de treinamento e engajamento. NPS acima de 90%, garantindo experiência excepcional para o cliente.",
+      },
+    ],
+    softSkills: [
+      "Gestão operacional",
+      "Liderança",
+      "Resolução de problemas",
+      "Contratação  de pessoas",
+    ],
+  },
+  {
+    id: 2,
+    role: "Gerente Assistente",
+    company: "Arcos Dourados - Mc Donald's",
+    startDate: "2012-01",
+    endDate: "2014-12",
+    workType: "Integral",
+    location: "São Paulo, SP",
+    companyImage: "/logo-arcos.png",
+    description:
+      "Gestão de programas de capacitação e integração para formação de equipes de alta performance, garantindo padronização e melhoria contínua.",
+    resultTitles: "Resultados obtidos:",
+    results: [
+      {
+        primary:
+          "+15% em produtividade com implementação de treinamentos direcionados.",
+      },
+      {
+        secondary:
+          "Erros operacionais reduzidos em 20% por padronização de processos.",
+      },
+      {
+        tertiary:
+          "Apoio na gestão financeira, controle de inventário e planejamento de mão de obra.",
+      },
+    ],
+    softSkills: ["treinamento", "gestão equipes"],
+  },
+  {
+    id: 3,
+    role: "Gerente Plantão",
+    company: "Arcos Dourados - Mc Donald's",
+    startDate: "2007-09",
+    endDate: "2012-01",
+    workType: "Integral",
+    location: "São Paulo, SP",
+    companyImage: "/logo-arcos.png",
+    description:
+      "Garantia dos resultados do restaurante durante o plantão, com foco em excelência operacional, vendas e atendimento ao cliente.",
+    resultTitles: "Principais entregas:",
+    results: [
+      {
+        primary: "+12% no ticket médio através de estratégias de vendas.",
+      },
+      {
+        secondary:
+          "Controle de inventário, qualidade e segurança alimentar (100% conformidade em auditorias).",
+      },
+      {
+        tertiary:
+          "Acompanhamento de treinamentos e correção de procedimentos em tempo real.",
+      },
+    ],
+    softSkills: ["gestão de vendas", "gestão de pessoas"],
+  },
+];
