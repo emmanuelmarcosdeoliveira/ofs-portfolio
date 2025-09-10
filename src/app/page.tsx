5c8772bc46f080f618c47785c7afdcc35be99621
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-09-09 23:18:22 -0300

    chore: remove variável isLoading do componente Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 import Experiences from "@/components/core-components/experiences";
 import HeroSection from "@/components/core-components/hero-section";
 import Projects from "@/components/core-components/Projects";
-
 import CardServices from "@/components/core-components/card-services";
 import CardTechnologies from "@/components/core-components/card-technologies";
 import LogoLoopComponents from "@/components/logos";
-
-const isLoading = true;
 
 export default function Home() {
   return (
