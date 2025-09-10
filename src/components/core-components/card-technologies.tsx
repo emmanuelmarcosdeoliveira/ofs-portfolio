b5ce38fa1bcceac974d63b367498fbcad3b506a8
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-09-09 22:24:55 -0300

    styles(components): changed the style of the technology card by removing the tool area

--- src/components/core-components/card-technologies.tsx
+++ src/components/core-components/card-technologies.tsx
@@ -68,33 +68,6 @@ export default function CardTechnologies() {
             ))}
           </div>
         </div>
-
-        <div className="md:-rotate-2">
-          <h1 className="text-primary font-serif pt-6 text-2xl">Ferramentas</h1>
-          <div className="grid py-6 grid-cols-3 md:grid-cols-10 grid-rows-2 gap-2">
-            {skills.tools.map((tec) => (
-              <div
-                key={tec.id}
-                className="grid cursor-pointer  grayscale-100 hover:grayscale-0   z-10 relative first:col-span-2 first:row-span-3    group transition-transform rounded-xl overflow-hidden   bg-neutral-300 dark:bg-neutral-800  ease-in-out dark:hover:bg-linear-to-b  shadow-xs  to-black  justify-center items-center"
-              >
-                <Link href={tec.url} target="_blank">
-                  <div className="p-6">
-                    <Image
-                      className="w-full group-hover:scale-110 transition-transform ease-in-out object-contain"
-                      src={tec.icon}
-                      alt=""
-                      width={90}
-                      height={0}
-                    />
-                    <p className="absolute max-sm:text-xs flex font-semibold bg-background/80 rounded-md  bottom-2 left-1 px-1 z-50 group-hover:opacity-100 opacity-0">
-                      {tec.name}
-                    </p>
-                  </div>
-                </Link>
-              </div>
-            ))}
-          </div>
-        </div>
       </div>
     </section>
   );
---
+++ src/constants/skills.ts
@@ -0,0 +1,192 @@
+export const skills = {
+  frontEnd: [
+    { id: 1, name: "React", icon: "/react.svg", url: "https://react.dev/" },
+    { id: 2, name: "Next", icon: "/next.svg", url: "https://nextjs.org/" },
+
+    {
+      id: 3,
+      name: "Tailwind CSS",
+      icon: "/tailwind.svg",
+      url: "https://tailwindcss.com/",
+    },
+    {
+      id: 4,
+      name: "TypeScript",
+      icon: "/typescript.svg",
+      url: "https://www.typescriptlang.org/",
+    },
+    {
+      id: 5,
+      name: "JavaScript",
+      icon: "/javascript.svg",
+      url: "https://developer.mozilla.org/pt-BR/docs/Web/JavaScript",
+    },
+    {
+      id: 6,
+      name: "HTML5",
+      icon: "/html.svg",
+      url: "https://developer.mozilla.org/pt-BR/docs/Web/CSS",
+    },
+    {
+      id: 7,
+      name: "CSS3",
+      icon: "/css.svg",
+      url: "https://developer.mozilla.org/pt-BR/docs/Web/CSS",
+    },
+    {
+      id: 8,
+      name: "styled components",
+      icon: "/styled.svg",
+      url: "https://styled-components.com/",
+    },
+    {
+      id: 9,
+      name: "SASS",
+      icon: "/sass.svg",
+      url: "https://sass-lang.com/",
+    },
+    {
+      id: 10,
+      name: "motion",
+      icon: "/motion.svg",
+      url: "https://motion.dev/",
+    },
+    {
+      id: 11,
+      name: "shadcn",
+      icon: "/shadcn.svg",
+      url: "https://ui.shadcn.com/",
+    },
+    {
+      id: 12,
+      name: "Better Auth",
+      icon: "/better.svg",
+      url: "https://www.better-auth.com/",
+    },
+
+    {
+      id: 13,
+      name: "cypress",
+      icon: "/cypress.svg",
+      url: "https://www.cypress.io/",
+    },
+    {
+      id: 14,
+      name: "Jest",
+      icon: "/jest.svg",
+      url: "https://jestjs.io/",
+    },
+    { id: 15, name: "Vercel", icon: "/vercel.svg", url: "https://vercel.com/" },
+    {
+      id: 16,
+      name: "netlifly",
+      icon: "/netlifly.svg",
+      url: "https://www.netlify.com/",
+    },
+    {
+      id: 17,
+      name: "Clerk",
+      icon: "/clerk.svg",
+      url: "https://clerk.com/",
+    },
+    {
+      id: 18,
+      name: "Vite",
+      icon: "/vite.svg",
+      url: "https://vite.dev/",
+    },
+    {
+      id: 19,
+      name: "Vue",
+      icon: "/vue.svg",
+      url: "https://vuejs.org/",
+    },
+    { id: 20, name: "git", icon: "/git.svg", url: "https://git-scm.com/" },
+    { id: 21, name: "gitHub", icon: "/gitHub.svg", url: "https://github.com/" },
+
+    {
+      id: 22,
+      name: "VsCode",
+      icon: "/vsCode.svg",
+      url: "https://code.visualstudio.com/",
+    },
+
+    {
+      id: 23,
+      name: "Figma",
+      icon: "/figma.svg",
+      url: "https://www.figma.com/",
+    },
+    { id: 24, name: "n8n", icon: "/n8n.svg", url: "https://n8n.io/" },
+  ],
+  backEnd: [
+    {
+      id: 1,
+      name: "Node.js",
+      icon: "/node.svg",
+      url: "https://nodejs.org/pt",
+    },
+    {
+      id: 2,
+      name: "Express",
+      icon: "/express.svg",
+      url: "https://expressjs.com/",
+    },
+
+    {
+      id: 3,
+      name: "PostgreSQL",
+      icon: "/postgres.svg",
+      url: "https://www.postgresql.org/",
+    },
+    {
+      id: 4,
+      name: "Prisma",
+      icon: "/prisma.svg",
+      url: "https://www.prisma.io",
+    },
+
+    {
+      id: 5,
+      name: "SQLite",
+      icon: "/sqlite.svg",
+      url: "https://www.sqlite.org/",
+    },
+    {
+      id: 6,
+      name: "Drizzle ORM",
+      icon: "/drizzle.svg",
+      url: "https://orm.drizzle.team/",
+    },
+    {
+      id: 7,
+      name: "Neon",
+      icon: "/neon.svg",
+      url: "https://neon.com/",
+    },
+    {
+      id: 8,
+      name: "SupaBase",
+      icon: "/supabase.svg",
+      url: "https://supabase.com/",
+    },
+    {
+      id: 9,
+      name: "Docker",
+      icon: "/docker.svg",
+      url: "https://www.docker.com/",
+    },
+    {
+      id: 10,
+      name: "Fastify",
+      icon: "/fastify.svg",
+      url: "https://fastify.dev/",
+    },
+    {
+      id: 11,
+      name: "Render",
+      icon: "/render.svg",
+      url: "https://render.com/",
+    },
+  ],
+};
