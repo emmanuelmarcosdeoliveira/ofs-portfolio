1b88cf12753fadbfd265e9b92e4bbb95d075a1cf
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-09-12 11:57:05 -0300

    refactor: update project structure and styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import Experiences from "@/components/core-components/experiences";
-import HeroSection from "@/components/core-components/hero-section";
-import Projects from "@/components/core-components/Projects";
 import CardServices from "@/components/core-components/card-services";
 import CardTechnologies from "@/components/core-components/card-technologies";
+import HeroSection from "@/components/core-components/hero-section";
+import Projects from "@/components/core-components/Projects";
+import Experiences from "@/components/experiences";
 import LogoLoopComponents from "@/components/logos";
 
 export default function Home() {
   return (
-    <main className="overflow-x-hidden px-4">
+    <main className=" overflow-hidden relative px-4">
       <HeroSection />
       <Projects />
       <CardServices />
