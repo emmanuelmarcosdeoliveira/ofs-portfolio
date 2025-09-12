978c0f5fa35b4f011a6d6e17a454363dcc154f29
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-09-12 11:45:51 -0300

    chore: animation standardization in a single file

---
+++ src/styles/animationMotion.ts
@@ -0,0 +1,20 @@
+export const fadeUp = {
+  initial: { opacity: 0, y: -100 },
+  whileInView: { opacity: 1, y: 0 },
+  exit: { opacity: 0, y: -100 },
+};
+export const fadeUpDelay = {
+  initial: { opacity: 0, y: -100 },
+  whileInView: { opacity: 1, y: 0 },
+  exit: { opacity: 0, y: -100 },
+};
+export const fadeLeft = {
+  initial: { opacity: 0, x: 100 },
+  whileInView: { opacity: 1, x: 0 },
+  exit: { opacity: 0, x: 100 },
+};
+export const fadeRight = {
+  initial: { opacity: 0, x: -100 },
+  whileInView: { opacity: 1, x: 0 },
+  exit: { opacity: 0, x: -100 },
+};
